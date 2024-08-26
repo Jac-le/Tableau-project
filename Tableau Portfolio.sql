a3c5bfae328a68aaf3ea26b59ae4861c24377f74
Jac-le <mahubanej111@gmail.com>
2024-08-26 02:27:49 +0200

    Add files via upload

---
+++ Tableau Portfolio.sql
@@ -0,0 +1,60 @@
+/*
+
+Queries used for Tableau Project
+
+*/
+
+
+
+-- 1. 
+--Percentage on Cases
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From CovidDeaths$
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+ 
+
+
+-- 2. 
+
+--  Death Count
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From CovidDeaths$
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3.
+-- infectin Count Over Population
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeaths$
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4.
+-- Percent Populatoin Infected
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidDeaths$
+--Where location like '%states%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
+
+
+
+
+
+
+
+
+ 
+
